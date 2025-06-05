c63201f71b7756108c651ff683b353625ccc5c55
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-06-05 14:54:53 +0700

    #21. Create Schema (Model)

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,28 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+    @Prop({ required: true })
+    email: string;
+
+    @Prop({ require: true })
+    password: string;
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    address: string;
+
+    @Prop()
+    phone: number;
+
+    @Prop()
+    age: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
