8e9745f5a98bd1497b76f1c5b8603f7d8c72cee9
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-06-06 19:33:52 +0700

    #40. Implementing Passport JWT (JWT)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -17,5 +18,11 @@ export class AppController {
   @Post("/login")
   handleLogin(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
