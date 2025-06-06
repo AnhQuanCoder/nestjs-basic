2185c6447796093e650487c9322b639130a68270
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-06-06 12:50:53 +0700

    #36. Local Strategies với NestJS (JWT)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import mongoose, { Model } from 'mongoose';
-import { genSaltSync, hashSync } from "bcryptjs";
+import { compareSync, genSaltSync, hashSync } from "bcryptjs";
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) { }
@@ -41,6 +41,16 @@ export class UsersService {
 
     const user = this.userModel.findOne({ _id: id })
     return user;
+  }
+
+  findOneByUsername(username: string) {
+    return this.userModel.findOne({
+      email: username
+    })
+  }
+
+  isValidPassword(password: string, hash: string) {
+    return compareSync(password, hash);
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
