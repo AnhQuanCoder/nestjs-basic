558e6257b56ed384e6a754259797d03ccd822690
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-06-07 21:19:57 +0700

    #56. Passing Req.user (Customize decorator @User)

--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -1,4 +1,15 @@
-import { SetMetadata } from "@nestjs/common";
+import { createParamDecorator, ExecutionContext, SetMetadata } from "@nestjs/common";
 
+// @Public: Những routes nào không muốn JWT
 export const IS_PUBLIC_KEY = "isPublic";
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true); // key: value
+// End @Public
+
+// @User: Lấy thông tin user đăng nhập
+export const User = createParamDecorator(
+    (data: unknown, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest();
+        return request.user;
+    },
+);
+// End @User
