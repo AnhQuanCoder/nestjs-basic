604e66c57e523c4728b8cca45ad8bb6cd96456ee
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-06-08 16:35:20 +0700

    #63. Update User Schema, User DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,65 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { Type } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from "class-validator";
+import mongoose from "mongoose";
+
+class Company {
+    @IsNotEmpty()
+    _id: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty()
+    name: string;
+}
 
 export class CreateUserDto {
+    // Create User phía admin
+
+    @IsNotEmpty({ message: "Họ tên không được để trống" })
+    name: string;
+
     @IsNotEmpty({ message: "Email không được để trống" })
     @IsEmail({}, { message: "Email không đúng định dạng" })
     email: string;
 
     @IsNotEmpty({ message: "Password không được để trống" })
     password: string;
+
+    @IsNotEmpty({ message: "Tuổi không được để trống" })
+    age: string;
+
+    @IsNotEmpty({ message: "Giới tính không được để trống" })
+    gender: string;
+
+    @IsNotEmpty({ message: "Địa chỉ không được để trống" })
+    address: string;
+
+    @IsNotEmpty({ message: "Quyền không được để trống" })
+    role: string;
+
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => Company)
+    company: Company;
+}
+export class RegisterUserDto {
+    // Create User phía client
+
+    @IsNotEmpty({ message: "Họ tên không được để trống" })
     name: string;
+
+    @IsNotEmpty({ message: "Email không được để trống" })
+    @IsEmail({}, { message: "Email không đúng định dạng" })
+    email: string;
+
+    @IsNotEmpty({ message: "Password không được để trống" })
+    password: string;
+
+    @IsNotEmpty({ message: "Tuổi không được để trống" })
+    age: string;
+
+    @IsNotEmpty({ message: "Giới tính không được để trống" })
+    gender: string;
+
+    @IsNotEmpty({ message: "Địa chỉ không được để trống" })
     address: string;
 }
