5d6a3d664b80e49acdb163db5b66f9bef0d56f40
AnhQuanCoder <138649172+AnhQuanCoder@users.noreply.github.com>
2025-06-08 14:43:06 +0700

    #62. Version APIs (Cấu hình api version cho dự án)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 
@@ -25,6 +25,14 @@ async function bootstrap() {
   // Interceptor
   app.useGlobalInterceptors(new TransformInterceptor(reflector));
   // End Interceptor
+
+  // Config versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ["1", "2"]
+  });
+  // End config versioning
 
   app.useGlobalPipes(new ValidationPipe());
   const port = configService.get('PORT');
